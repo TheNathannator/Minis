a03e5c7e2f981aef4205db510169f00fb762db4c
Nathan <nathanhurst03@gmail.com>
2025-11-16 18:35:11 -0700

    Update RtMidi and imports

--- Packages/jp.keijiro.minis/Runtime/Native/RtMidi.cs
+++ Packages/jp.keijiro.minis/Runtime/Native/RtMidi.cs
@@ -3,6 +3,34 @@ using System;
 
 namespace Minis.Native
 {
+    public enum RtMidiApi
+    {
+        Unspecified,
+        MacOsXCore,
+        LinuxAlsa,
+        UnixJack,
+        WindowsMM,
+        RtMidiDummy,
+        WebMidiApi,
+        WindowsUwp,
+        Android
+    }
+
+    public enum RtMidiErrorType
+    {
+        Warning,
+        DebugWarning,
+        Unspecified,
+        NoDevicesFound,
+        InvalidDevice,
+        MemoryError,
+        InvalidParameter,
+        InvalidUse,
+        DriverError,
+        SystemError,
+        ThreadError
+    }
+
     internal unsafe abstract class RtMidiHandle : SafeHandle
     {
         [StructLayout(LayoutKind.Sequential)]
@@ -37,12 +65,9 @@ namespace Minis.Native
 
         protected override bool ReleaseHandle()
         {
-            rtmidi_in_free(handle);
+            RtMidi.rtmidi_in_free(handle);
             return true;
         }
-
-        [DllImport(RtMidi.DllName)]
-        private static extern void rtmidi_in_free(IntPtr device);
     }
 
     internal sealed class RtMidiOutHandle : RtMidiHandle
@@ -54,30 +79,71 @@ namespace Minis.Native
 
         protected override bool ReleaseHandle()
         {
-            rtmidi_out_free(handle);
+            RtMidi.rtmidi_out_free(handle);
             return true;
         }
-
-        [DllImport(RtMidi.DllName)]
-        private static extern void rtmidi_out_free(IntPtr device);
     }
 
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     internal unsafe delegate void RtMidiCCallback(
         double timeStamp,
-        byte* message,
-        UIntPtr messageSize,
+        byte* message, // const unsigned char*
+        UIntPtr messageSize, // size_t
         void* userData
     );
 
-    internal static unsafe class RtMidi
+    internal static class RtMidi
     {
-        internal const string DllName = "RtMidi.dll";
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_WEBGL)
+        internal const string DllName = "__Internal";
+#else
+        internal const string DllName = "RtMidi";
+#endif
+
+        #region System
+
+        [DllImport(DllName)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string rtmidi_get_version(); // -> const char*
+
+        [DllImport(DllName)]
+        public static unsafe extern int rtmidi_get_compiled_api(
+            RtMidiApi* apis,
+            uint apis_size
+        );
+
+        [DllImport(DllName)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string rtmidi_api_name( // -> const char*
+            RtMidiApi api
+        );
+
+        [DllImport(DllName)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string rtmidi_api_display_name( // -> const char*
+            RtMidiApi api
+        );
+
+        [DllImport(DllName)]
+        public static extern RtMidiApi rtmidi_compiled_api_by_name(
+            [MarshalAs(UnmanagedType.LPStr)] string name // const char*
+        );
+
+        #endregion
+
+        #region Ports
 
         [DllImport(DllName)]
         public static extern void rtmidi_open_port(
             RtMidiHandle device,
             uint portNumber,
-            [MarshalAs(UnmanagedType.LPStr)] string portName
+            [MarshalAs(UnmanagedType.LPStr)] string portName // const char*
+        );
+
+        [DllImport(DllName)]
+        public static extern void rtmidi_open_virtual_port(
+            RtMidiHandle device,
+            [MarshalAs(UnmanagedType.LPStr)] string portName // const char*
         );
 
         [DllImport(DllName)]
@@ -90,18 +156,60 @@ namespace Minis.Native
             RtMidiHandle device
         );
 
-        [DllImport(DllName)]
+        [DllImport(DllName, EntryPoint = "rtmidi_get_port_name")]
         [return: MarshalAs(UnmanagedType.LPStr)]
-        public static extern string rtmidi_get_port_name(
+        private static unsafe extern int _rtmidi_get_port_name(
             RtMidiHandle device,
-            uint portNumber
+            uint portNumber,
+            byte* bufOut, // char*
+            ref int bufLen // int*
         );
+
+        public static unsafe string rtmidi_get_port_name(RtMidiHandle device, uint portNumber)
+        {
+            int length = 0;
+            _rtmidi_get_port_name(device, portNumber, null, ref length);
+            if (!device.Ok)
+            {
+                return null;
+            }
+
+            byte* buffer = stackalloc byte[length];
+            int result = _rtmidi_get_port_name(device, portNumber, null, ref length);
+            if (!device.Ok)
+            {
+                return null;
+            }
+
+            return Marshal.PtrToStringAnsi((IntPtr)buffer);
+        }
+
+        #endregion
+
+        #region Input
 
         [DllImport(DllName)]
         public static extern RtMidiInHandle rtmidi_in_create_default();
 
         [DllImport(DllName)]
-        public static extern void rtmidi_in_set_callback(
+        public static extern RtMidiInHandle rtmidi_in_create(
+            RtMidiApi api,
+            [MarshalAs(UnmanagedType.LPStr)] string clientName, // const char*
+            uint queueSizeLimit
+        );
+
+        [DllImport(DllName)]
+        public static extern void rtmidi_in_free(
+            IntPtr device
+        );
+
+        [DllImport(DllName)]
+        static extern RtMidiApi rtmidi_in_get_current_api(
+            RtMidiInHandle device
+        );
+
+        [DllImport(DllName)]
+        public static unsafe extern void rtmidi_in_set_callback(
             RtMidiInHandle device,
             RtMidiCCallback callback,
             void* userData
@@ -113,19 +221,49 @@ namespace Minis.Native
         );
 
         [DllImport(DllName)]
-        public static extern double rtmidi_in_get_message(
+        public static extern void rtmidi_in_ignore_types(
             RtMidiInHandle device,
-            byte* message,
-            ref UIntPtr size
+            [MarshalAs(UnmanagedType.U1)] bool midiSysex,
+            [MarshalAs(UnmanagedType.U1)] bool midiTime,
+            [MarshalAs(UnmanagedType.U1)] bool midiSense
         );
+
+        [DllImport(DllName)]
+        public static unsafe extern double rtmidi_in_get_message(
+            RtMidiInHandle device,
+            byte* message, // unsigned char*
+            ref UIntPtr size // size_t*
+        );
+
+        #endregion
+
+        #region Output
+
+        #endregion
 
         [DllImport(DllName)]
         public static extern RtMidiOutHandle rtmidi_out_create_default();
 
         [DllImport(DllName)]
-        public static extern int rtmidi_out_send_message(
+        public static extern RtMidiOutHandle rtmidi_out_create(
+            RtMidiApi api,
+            [MarshalAs(UnmanagedType.LPStr)] string clientName // const char*
+        );
+
+        [DllImport(DllName)]
+        public static extern void rtmidi_out_free(
+            IntPtr device
+        );
+
+        [DllImport(DllName)]
+        public static extern RtMidiApi rtmidi_out_get_current_api(
+            RtMidiOutHandle device
+        );
+
+        [DllImport(DllName)]
+        public static unsafe extern int rtmidi_out_send_message(
             RtMidiOutHandle device,
-            byte* message,
+            byte* message, // const unsigned char*
             int length
         );
     }
